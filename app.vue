4b19572eb9b6a825954820bb7bc3dbffa8baab05
Tanya Kozlova <kozlovatana366@gmail.com>
2024-09-23 14:07:27 +0300

    remove nuxt loading, add catalog page

--- app.vue
+++ app.vue
@@ -1,21 +1,9 @@
 <template>
   <NuxtLayout name="defaultlayout">
-    <NuxtLoadingIndicator v-if="loading"/>
-    <Teleport to="body" v-if="loading">
-      <transition>
-        <div class="overlay">
-          Loading...
-        </div>
-      </transition>
-    </Teleport>
-    <NuxtPage v-if="!loading"/>
+    <NuxtPage />
   </NuxtLayout>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
 import '~/assets/styles/main.css'
-
-const loading = ref<boolean>(true)
-setTimeout(() => loading.value = false, 2000)
 </script>
